506ce3fa4af808915f0ea1b798c0e922e39ac816
Kay <152438149+Sokpe@users.noreply.github.com>
2025-07-19 15:42:35 +0000

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -5,15 +5,15 @@ operation = input("Choose the operation (+, -, *, /): ")
 match operation:
     case "+":
         result = num1 + num2
-        print(f"The result of {num1} + {num2} is {result}") 
+        print(f"The result is {num1} + {num2} is {result}") 
 
     case "-":
         result = num1 - num2
-        print(f"The result of {num1} - {num2} is {result}")
+        print(f"The result is {num1} - {num2} is {result}")
 
     case "*":
         result = num1 * num2
-        print(f"The result of {num1} * {num2} is {result}")
+        print(f"The result is {num1} * {num2} is {result}")
 
     case "/":
         if num2 != 0:
