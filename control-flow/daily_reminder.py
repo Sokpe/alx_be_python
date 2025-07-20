d464af309cdc79410cfa52f445d86a5641c40efa
Kay <152438149+Sokpe@users.noreply.github.com>
2025-07-20 16:16:55 +0000

    Add files via upload

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,16 @@
+task= input("Describe Your task for today: ").lower()
+task_priority= input("What is the priority of this task? (high, medium, low): ").lower()
+time_bound=input("Is this task time-bound? (yes or no): ").lower()
+match task_priority:
+      
+    case "high":
+        print(f"Your task: {task} is of high priority.")
+    case "medium":
+        print(f"Your task: {task} is of medium priority.")
+    case "low":
+        print(f"Your task: {task} is of low priority.")
+        
+        if time_bound == "yes":
+            print("Make sure to complete it soon.")
+
+    
