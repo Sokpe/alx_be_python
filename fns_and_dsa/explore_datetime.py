602f713dcfa714cf55c6ebc82181bdedd31d61ed
Kay <152438149+Sokpe@users.noreply.github.com>
2025-07-27 23:36:05 +0000

    Add files via upload

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current Date and Time: {formatted_date}")
+
+def calculate_future_date(days):
+    current_date = datetime.now()
+    future_date = current_date + timedelta(days=days)
+    formatted_date = future_date.strftime("%Y-%m-%d")
+    print(f"Future Date: {formatted_date}")
+
+def main():
+    display_current_datetime()
+    
+    try:
+        days = int(input("Enter the number of days to add: "))
+        calculate_future_date(days)
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
+
+if __name__ == "__main__":
+    main()
