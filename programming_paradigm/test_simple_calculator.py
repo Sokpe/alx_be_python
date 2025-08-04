552df7bd0d0ce8c9256633181c4691ac6c478863
Kay <152438149+Sokpe@users.noreply.github.com>
2025-08-04 15:23:20 +0000

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,25 +5,25 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(5, 3), 8)
         self.assertEqual(self.calc.add(-5, 3), -2)
         self.assertEqual(self.calc.add(-5, -3), -8)
         self.assertEqual(self.calc.add(0, 0), 0)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(-5, 3), -8)
         self.assertEqual(self.calc.subtract(-5, -3), -2)
         self.assertEqual(self.calc.subtract(0, 0), 0)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(5, 3), 15)
         self.assertEqual(self.calc.multiply(-5, 3), -15)
         self.assertEqual(self.calc.multiply(-5, -3), 15)
         self.assertEqual(self.calc.multiply(0, 5), 0)
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(-10, 2), -5)
         self.assertEqual(self.calc.divide(-10, -2), 5)
