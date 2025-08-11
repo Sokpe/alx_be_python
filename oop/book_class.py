9fae4401ba2be489b91714227577c81aa860f594
Kay <152438149+Sokpe@users.noreply.github.com>
2025-08-11 23:32:03 +0000

    Update book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -12,25 +12,3 @@ class Book:
 
     def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
-
-
-And here's the main.py script to test the Book class:
-
-
-from book_class import Book
-
-def main():
-    # Creating an instance of Book
-    my_book = Book("1984", "George Orwell", 1949)
-
-    # Demonstrating the __str__ method
-    print(my_book)  # Expected to use __str__
-
-    # Demonstrating the __repr__ method
-    print(repr(my_book))  # Expected to use __repr__
-
-    # Deleting a book instance to trigger __del__
-    del my_book
-
-if __name__ == "__main__":
-    main()
